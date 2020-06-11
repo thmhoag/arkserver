f86868551256c6411d4de6f6814f72b004465e35
Thomas Hoag <thmhoag@gmail.com>
2020-06-11 15:57:20 -0400

    Move cleanup to end of run and only cleanup instance related files

--- run.sh
+++ run.sh
@@ -15,18 +15,6 @@ sudo chown steam:steam -R /ark
 
 [ -n "$defaultinstance" ] || { echo "defaultinstance is required and not set, exiting..." >&2; exit 1; }
 
-# Remove arkmanager tracking files for this instance if they exist
-# They can cause issues with starting the server multiple times
-# due to the restart command not completing when the container exits
-echo "Cleaning up any leftover arkmanager files..."
-[ -f "/ark/server/ShooterGame/Saved/.ark-warn-${defaultinstance}.lock" ] && rm -rf "/ark/server/ShooterGame/Saved/.ark-warn-${defaultinstance}.lock"
-[ -f "/ark/server/ShooterGame/Saved/.ark-update.lock" ] && rm -rf "/ark/server/ShooterGame/Saved/.ark-update.lock"
-[ -f "/ark/server/ShooterGame/Saved/.ark-update.time" ] && rm -rf "/ark/server/ShooterGame/Saved/.ark-update.time"
-[ -f "/ark/server/ShooterGame/Saved/.arkmanager-${defaultinstance}.pid" ] && rm -rf "/ark/server/ShooterGame/Saved/.arkmanager-${defaultinstance}.pid"
-[ -f "/ark/server/ShooterGame/Saved/.arkserver-${defaultinstance}.pid" ] && rm -rf "/ark/server/ShooterGame/Saved/.arkserver-${defaultinstance}.pid"
-[ -f "/ark/server/ShooterGame/Saved/.autorestart" ] && rm -rf "/ark/server/ShooterGame/Saved/.autorestart"
-[ -f "/ark/server/ShooterGame/Saved/.autorestart-${defaultinstance}" ] && rm -rf "/ark/server/ShooterGame/Saved/.autorestart-${defaultinstance}"
-
 # Create directories if they don't exist
 [ ! -d /ark/config ] && mkdir /ark/config
 [ ! -d /ark/log ] && mkdir /ark/log
@@ -120,7 +108,6 @@ else
 	echo "Backup on start is not enabled."
 fi
 
-
 function stop {
 	arkmanager broadcast "Server is shutting down"
 	arkmanager notify "Server is shutting down"
@@ -139,3 +126,15 @@ trap stop TERM
 arkmanager start --no-background --verbose &
 arkmanpid=$!
 wait $arkmanpid
+
+# Remove arkmanager tracking files for this instance if they exist
+# They can cause issues with starting the server multiple times
+# due to the restart command not completing when the container exits
+echo "Cleaning up any leftover arkmanager files..."
+[ -f "/ark/server/ShooterGame/Saved/.ark-warn-${defaultinstance}.lock" ] && rm -rf "/ark/server/ShooterGame/Saved/.ark-warn-${defaultinstance}.lock"
+[ -f "/ark/server/ShooterGame/Saved/.arkmanager-${defaultinstance}.pid" ] && rm -rf "/ark/server/ShooterGame/Saved/.arkmanager-${defaultinstance}.pid"
+[ -f "/ark/server/ShooterGame/Saved/.arkserver-${defaultinstance}.pid" ] && rm -rf "/ark/server/ShooterGame/Saved/.arkserver-${defaultinstance}.pid"
+[ -f "/ark/server/ShooterGame/Saved/.autorestart-${defaultinstance}" ] && rm -rf "/ark/server/ShooterGame/Saved/.autorestart-${defaultinstance}"
+# [ -f "/ark/server/ShooterGame/Saved/.autorestart" ] && rm -rf "/ark/server/ShooterGame/Saved/.autorestart"
+# [ -f "/ark/server/ShooterGame/Saved/.ark-update.lock" ] && rm -rf "/ark/server/ShooterGame/Saved/.ark-update.lock"
+# [ -f "/ark/server/ShooterGame/Saved/.ark-update.time" ] && rm -rf "/ark/server/ShooterGame/Saved/.ark-update.time"
